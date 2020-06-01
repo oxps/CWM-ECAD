185f092eab7c5a3cc2350ad9ebb5e33bb9eb9bd1
Cloud User <centos@ip-172-31-7-197.eu-west-2.compute.internal>
2020-06-01 15:51:49 +0000

    Incomplete solution to Ex4

--- Ex4/top.v
+++ Ex4/top.v
@@ -16,3 +16,34 @@
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module diceroll(
+    input clk,
+    input rst,
+    input button,
+    output reg [2:0] throw
+    );
+
+    wire out;
+
+    always @(posedge clk or posedge rst)
+    begin
+	if (rst==1)
+	   throw<=3'b001; 
+	else if(button==0)
+	   throw<=throw;
+        else if(button==1)
+           throw<=throw+1;
+	   begin //errors here
+		if(throw==3'b000)
+                    throw<=1;
+	        else if(throw==3'b111)
+	            throw<=1;
+                else
+                    throw<=throw+1;
+    	   end
+    end
+
+
+endmodule
--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -7,3 +7,53 @@
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module top_tb(
+    );
+    
+    //Todo: Parameters
+    parameter CLK_PERIOD = 10;
+
+    //Todo: Registers and wires
+    reg clk;
+    reg rst;
+    reg button;
+
+    wire [2:0] throw;
+
+    //Todo: Clock generation
+    initial
+    begin
+       clk = 1'b0;
+       forever
+         #(CLK_PERIOD/2) clk=~clk;
+    end
+
+    //Todo: User logic
+    initial begin
+       rst=1;
+       button=1;
+       #10
+       rst=0;
+       #100
+       button=0;
+    end
+
+    //An initial block that ends the simulation, and declares success (if valid)
+    initial begin
+       #150
+       if (throw!=0)
+          $display("Your dice roll is: %d",throw);
+       $finish;
+    end
+     
+    //Todo: Instantiate counter module
+    diceroll top (
+    .clk (clk),
+    .rst (rst),
+    .button (button),
+    .throw (throw)
+    );
+ 
+endmodule 
