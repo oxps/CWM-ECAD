5aef587023ecdd26301db836f61ee35b2cf08404
Cloud User <centos@ip-172-31-7-197.eu-west-2.compute.internal>
2020-06-01 14:48:00 +0000

    A complete solution to Ex3

--- Ex3/top.v
+++ Ex3/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #3 
-// Student Name:
-// Date: 
+// Student Name: Payas Sinha
+// Date: 01/06/2020
 //
 //  Description: In this exercise, you need to design an up / down counter, where 
 //  if the rst=1, the counter should be set to zero. If enable=0, the value
@@ -20,11 +20,27 @@
 
 module counter(
     //Todo: add ports 
-
+    input clk,
+    input rst,
+    input enable,
+    input direction,
+    output reg [7:0] counter_out
     );
                     
     //Todo: add registers and wires, if needed
+    wire out;
 
     //Todo: add user logic
-      
+    always @(posedge clk or posedge rst)
+    begin
+	if (rst==1)
+	   counter_out<=8'b0;  	
+	else if (enable==0)
+	   counter_out<=counter_out;
+	else if (direction==1)
+	   counter_out<=counter_out+1;
+	else 
+	   counter_out<=counter_out-1;
+    end
+    
 endmodule
--- Ex3/top_tb.v
+++ Ex3/top_tb.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #3 - Up/Down counter
-// Student Name:
-// Date: 
+// Student Name: Payas Sinha
+// Date: 01/06/2020
 //
 // Description: A testbench module to test Ex3 - counter
 // Guidance: start with simple tests of the module (how should it react to each 
@@ -13,16 +13,84 @@
 module top_tb(
     );
     
-//Todo: Parameters
+    //Todo: Parameters
+    parameter CLK_PERIOD = 10;
 
-//Todo: Regitsers and wires
+    //Todo: Registers and wires
+    reg clk;
+    reg rst;
+    reg enable;
+    reg direction;
+    reg err;
 
-//Todo: Clock generation
+    wire [7:0] counter_out;
 
-//Todo: User logic
-    
-//Todo: Finish test, check for success
+    //Todo: Clock generation
+    initial
+    begin
+       clk = 1'b0;
+       forever
+         #(CLK_PERIOD/2) clk=~clk;
+     end
 
-//Todo: Instantiate counter module
+    //Todo: User logic
+    initial begin
+	err=0;	
+	rst=1;
+	enable=1;
+	direction=1;
+	#20
+	if(counter_out!=0)
+        begin //test for initialisation
+          $display("***TEST FAILED! INITIALISATION INCORRECT! counter_out==0, actual counter_out=%d ***",counter_out);
+          err=1;
+        end
+	rst=0;
+	#60
+	if(counter_out!=6)
+        begin //test for counting up
+          $display("***TEST FAILED! COUNT UP NOT WORKING! counter_out==2, actual counter_out=%d ***",counter_out);
+          err=1;
+        end
+	enable=0;
+	#20
+	if(counter_out!=6)
+        begin //test for enable and disable features
+          $display("***TEST FAILED! DISABLE COUNTER NOT WORKING! counter_out==2, actual counter_out=%d ***",counter_out);
+          err=1;
+        end
+	enable=1;
+	direction=0;
+	#30
+	if(counter_out!=3)
+        begin //test for counting down
+          $display("***TEST FAILED! COUNT DOWN NOT WORKING! counter_out==1, actual counter_out=%d ***",counter_out);
+          err=1;
+        end
+	rst=1;
+	#20
+	if(counter_out!=0)
+        begin //test for resetting
+          $display("***TEST FAILED! RESET NOT WORKING! counter_out==0, actual counter_out=%d ***",counter_out);
+          err=1;
+        end
+    end
+
+    //Todo: Finish test, check for success
+    initial begin
+       #150 
+       if (err==0)
+         $display("***TEST PASSED! :) ***");
+       $finish;
+     end
+
+    //Todo: Instantiate counter module
+    counter top (
+    .clk (clk),
+    .rst (rst),
+    .enable (enable),
+    .direction (direction),
+    .counter_out (counter_out)
+    );
  
 endmodule 
