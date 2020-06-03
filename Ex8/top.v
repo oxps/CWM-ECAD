9c3ebc2691cbfd3233d39b8e892711bc9554a60b
Cloud User <centos@ip-172-31-7-197.eu-west-2.compute.internal>
2020-06-03 15:21:34 +0000

    An incomplete solution to Ex8

--- Ex8/top.v
+++ Ex8/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #8
-// Student Name:
-// Date: 
+// Student Name: Payas Sinha
+// Date: 03/06/2020
 //
 //  Description: In this exercise, you need to implement a times table of 0..7x0..7
 //  using a memory and AXI-4-lite interface.
@@ -12,3 +12,67 @@
 //  outputs:
 //           result[5:0]
 //////////////////////////////////////////////////////////////////////////////////
+
+`timescale 1ns / 100ps
+
+module multiplier(
+	//Define inputs
+	input clk,
+	input [2:0] a,
+	input [2:0] b,
+	input read,
+	input rst,
+	output [5:0] result
+	);
+
+	//Define output axi4 registers
+	wire rsta_busy;
+	wire rstb_busy;
+	wire s_axi_awready;
+	wire s_axi_wready;
+	wire [1:0] s_axi_bresp;
+	wire s_axi_bvalid;
+	wire s_axi_arready;
+	wire [31:0] s_axi_rdata;
+	wire [1:0] s_axi_rresp;
+	wire s_axi_rvalid;
+
+	//Define input axi4 registers
+	wire [31:0] s_axi_awaddr;
+	wire [31:0] s_axi_wdata;
+	wire [31:0] s_axi_araddr;
+    wire [3:0] s_axi_wstrb;
+
+	//Logic
+	assign s_axi_araddr[31:0]=(a*8)+b;
+	assign s_axi_awaddr[31:0]=1;
+	assign s_axi_wdata[31:0]=1;
+	assign s_axi_wstrb[3:0]=1;
+	assign result[5:0]=s_axi_rdata[5:0];
+
+
+axi4_multiplier your_instance_name (
+  .rsta_busy(rsta_busy),          // output wire rsta_busy
+  .rstb_busy(rstb_busy),          // output wire rstb_busy
+  .s_aclk(clk),                // input wire s_aclk
+  .s_aresetn(rst),          // input wire s_aresetn
+  .s_axi_awaddr(s_axi_awaddr),    // input wire [31 : 0] s_axi_awaddr
+  .s_axi_awvalid(read),  // input wire s_axi_awvalid
+  .s_axi_awready(s_axi_awready),  // output wire s_axi_awready
+  .s_axi_wdata(s_axi_wdata),      // input wire [31 : 0] s_axi_wdata
+  .s_axi_wstrb(s_axi_wstrb),      // input wire [3 : 0] s_axi_wstrb
+  .s_axi_wvalid(read),    // input wire s_axi_wvalid
+  .s_axi_wready(s_axi_wready),    // output wire s_axi_wready
+  .s_axi_bresp(s_axi_bresp),      // output wire [1 : 0] s_axi_bresp
+  .s_axi_bvalid(s_axi_bvalid),    // output wire s_axi_bvalid
+  .s_axi_bready(read),    // input wire s_axi_bready
+  .s_axi_araddr(s_axi_araddr),    // input wire [31 : 0] s_axi_araddr
+  .s_axi_arvalid(read),  // input wire s_axi_arvalid
+  .s_axi_arready(s_axi_arready),  // output wire s_axi_arready
+  .s_axi_rdata(s_axi_rdata),      // output wire [31 : 0] s_axi_rdata
+  .s_axi_rresp(s_axi_rresp),      // output wire [1 : 0] s_axi_rresp
+  .s_axi_rvalid(s_axi_rvalid),    // output wire s_axi_rvalid
+  .s_axi_rready(read)    // input wire s_axi_rready
+);
+
+endmodule
--- Ex8/top_tb.v
+++ Ex8/top_tb.v
@@ -1,9 +1,93 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #8 - Times Table using AXI interface
-// Student Name:
-// Date: 
+// Student Name: Payas Sinha
+// Date: 03/06/2020
 //
 // Description: A testbench module to test Ex8 - Times Table using AXI4-Lite
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module top_tb(
+    );
+    
+	//Parameters
+	parameter CLK_PERIOD = 10;
+
+	//Registers and wires
+	reg clk;
+	reg [2:0] a;
+	reg [2:0] b;
+	reg read;
+	reg rst;
+	reg err;
+
+	wire [5:0] result;
+
+	//Clock generation
+	initial
+	begin
+	clk = 1'b0;
+	forever
+		#(CLK_PERIOD/2) clk=~clk;
+	end
+
+	//Testing
+	initial begin
+		read=1;
+		rst=0;
+		err=0;
+		a<=3'd5;
+		b<=3'd6;
+		#20
+		if(result!=6'd30)
+      		begin //Test for alternating between dice and button
+          		$display("***TEST FAILED! 5x6=30! result==30, actual result=%d ***",result);
+          		err=1;
+       		end
+		a<=3'd2;
+		b<=3'd6;
+		#20
+		if(result!=6'd12)
+      		begin //Test for alternating between dice and button
+          		$display("***TEST FAILED! 2x6=12! result==12, actual result=%d ***",result);
+          		err=1;
+       		end
+		a<=3'd3;
+		b<=3'd5;
+		read=0;
+		#20
+		if(result!=6'd12)
+      		begin //Test for alternating between dice and button
+          		$display("***TEST FAILED! CHECK DISABLE FUNCTIONALITY! result==12, actual result=%d ***",result);
+          		err=1;
+       		end
+		read=1;
+		#20
+		if(result!=6'd15)
+      		begin //Test for alternating between dice and button
+          		$display("***TEST FAILED! CHECK ENABLE FUNCTIONALITY! result==15, actual result=%d ***",result);
+          		err=1;
+       		end
+	end
+		 
+	//Finish simulation and check for success
+	initial begin
+	#150
+	if (err==0)
+		$display("***TEST PASSED! YOUR RESULT IS: %d***", result);
+	$finish;
+	end
+
+	//Instantiate module
+	multiplier top(
+		.clk(clk),
+		.a(a),
+		.b(b),
+		.read(read),
+		.rst(rst),
+		.result(result)
+	);
+     
+endmodule 
