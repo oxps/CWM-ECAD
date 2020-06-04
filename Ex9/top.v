4d68f5c720fa3d91a9a1cba27bd5f4a4e357fbff
Cloud User <centos@ip-172-31-7-197.eu-west-2.compute.internal>
2020-06-04 15:15:31 +0000

    An incomplete solution to Ex9

--- Ex9/top.v
+++ Ex9/top.v
@@ -15,28 +15,53 @@
 
 
 module top(
-    input clk_p,
-    input clk_n,
-     //Todo: add all other ports besides clk_n and clk_p 
-   );
+	input clk_p,
+	input clk_n,
+	//Todo: add all other ports besides clk_n and clk_p
+	input rst_n,
+	input button,
+	output led_0,
+	output led_1,
+	output led_2
+	);
     
 
-   /* clock infrastructure, do not modify */
-        wire clk_ibufds;
+	/* clock infrastructure, do not modify */
+	wire clk_ibufds;
 
-    IBUFDS IBUFDS_sysclk (
-	.I(clk_p),
-	.IB(clk_n),
-	.O(clk_ibufds)
-);
+	IBUFDS IBUFDS_sysclk (
+		.I(clk_p),
+		.IB(clk_n),
+		.O(clk_ibufds)
+	);
 
-     wire clk; //use this signal as a clock for your design
+	wire clk; //use this signal as a clock for your design
         
-     BUFG bufg_clk (
-	.I  (clk_ibufds),
-	.O  (clk)
-      );
+	BUFG bufg_clk (
+		.I  (clk_ibufds),
+		.O  (clk)
+	);
 
-//Add logic here
+	//Add logic here
+	reg [32:0] counter;
+	reg state0, state1, state2;
+
+	assign led_0=state0;
+	assign led_1=state1;
+	assign led_2=state2;
+
+	always @(posedge clk)
+	if (button==1'b1) begin
+		state0<=1'b0;
+		state1<=1'b0;
+		state2<=1'b0;
+		end
+	else begin
+		counter<=counter+1;
+		state0<=counter[20];
+		state1<=counter[20];
+		state2<=counter[20];
+	end
+	
 
 endmodule
